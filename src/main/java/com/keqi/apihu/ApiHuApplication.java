e4cf6071c749189d1e894044e041b9ed4760c6de
iskeqi <iskeqi@126.com>
2020-10-25 20:24:00 +0800

    API 管理平台 Demo

---
+++ src/main/java/com/keqi/apihu/ApiHuApplication.java
@@ -0,0 +1,13 @@
+package com.keqi.apihu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiHuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiHuApplication.class, args);
+    }
+
+}
